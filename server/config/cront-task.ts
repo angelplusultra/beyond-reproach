37f143ff3132b3c09dacc77973aeb258c5eac04c
Mac Fitton <macfittondev@gmail.com>
2023-06-20 05:16:36 -0700

    Configured CRON job support, added purgeCarts CRON job

---
+++ server/config/cront-task.ts
@@ -0,0 +1,23 @@
+export default {
+  purgeCarts: {
+    task: async ({ strapi }: { strapi: Strapi.Strapi }) => {
+      await strapi.db.query('api::cart-item-meal.cart-item-meal').deleteMany({});
+
+      await strapi.db.query('api::cart-item-salad.cart-item-salad').deleteMany({});
+
+      await strapi.db.query('api::cart-item-snack.cart-item-snack').deleteMany({});
+
+      await strapi.db.query('api::cart-item-bundle.cart-item-bundle').deleteMany({});
+
+      strapi.log.info('Carts purged');
+    },
+    options: {
+      // cron string scheduling
+
+      // every 5 minutes
+      // cron: '*/5 * * * *',
+      rule: '*/20 * * * * *',
+      tz: 'America/New_York'
+    }
+  }
+};
--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,14 @@
+import crontTask from './cront-task';
+
 /*eslint-disable */
+
 export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: {
+    enabled: true,
+    tasks: crontTask
+  },
   app: {
     keys: env.array('APP_KEYS')
   },
