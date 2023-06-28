2bde6e5a34d467a764dd9cd157c63398c96fe50a
Mac Fitton <macfittondev@gmail.com>
2023-06-27 18:39:09 -0700

    Removed sendEmails test CRON job

--- server/config/cront-task.ts
+++ server/config/cront-task.ts
@@ -20,23 +20,5 @@ export default {
       rule: '*/20 * * * * *',
       tz: 'America/New_York'
     }
-  },
-  sendEmails: {
-    task: async ({ strapi }: { strapi: Strapi.Strapi }) => {
-      // const emails = ['macfittondev@gmail.com', 'marcellofitton@gmail.com', 'lareye1996@gmail.com'];
-      // await Promise.all(
-      //   emails.map(async (email) => {
-      //     await strapi.plugins['email'].services.email.send({
-      //       to: email,
-      //       subject: 'Test Email',
-      //       text: 'Hello world!',
-      //       html: 'Hello world!'
-      //     });
-      //   })
-      // );
-    },
-    options: {
-      rule: '*/40 * * * * *'
-    }
   }
 };
