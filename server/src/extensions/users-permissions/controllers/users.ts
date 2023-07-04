7c61f614b2fe6908f610b2f94346971e154be12f
Mac Fitton <macfittondev@gmail.com>
2023-07-04 11:59:44 -0700

    Added try catch to register controller adn error handling  for user permissions

--- server/src/extensions/users-permissions/controllers/users.ts
+++ server/src/extensions/users-permissions/controllers/users.ts
@@ -93,7 +93,6 @@ export default {
       try {
         await getService('user').sendConfirmationEmail(sanitizedUser);
       } catch (err) {
-        // ! KEEP AN EYE ON THIS
         if (err instanceof Error) {
           throw new ApplicationError(err.message);
         }
@@ -104,57 +103,66 @@ export default {
 
     const jwt = getService('jwt').issue(_.pick(user, ['id']));
 
-    const customer = await stripe.customers.create({
-      email: sanitizedUser.email,
-      name: sanitizedUser.username,
-      address: {
-        line1: sanitizedUser.street,
-        city: sanitizedUser.city,
-        postal_code: sanitizedUser.zipcode,
-        state: sanitizedUser.state
-      },
-      phone: sanitizedUser.mobile_number,
-      metadata: {
-        user_id: sanitizedUser.id
-      }
-    });
-
-    const session = await stripe.checkout.sessions.create({
-      mode: 'subscription',
-      customer: customer.id,
-      line_items: [{ price: process.env.STRIPE_TEST_MEMBERSHIP_PLAN_PRICE_ID, quantity: 1 }],
-      payment_method_types: ['paypal', 'card'],
-      payment_method_collection: 'always',
-      discounts: [{ coupon: process.env.STRIPE_TEST_MEMBERSHIP_PLAN_DISCOUNT_ID }],
-      currency: 'USD',
-      success_url: `${
-        process.env.SERVER_BASE_URL || 'http://localhost:1337'
-      }/api/auth/membership/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: 'http://localhost:1337',
-      metadata: {
-        user_id: sanitizedUser.id
-      }
-    });
     const users = strapi.db.query('plugin::users-permissions.user');
-    // TODO SESSION LINK AND JWT, FRONTEND WILL PUT JWT IN LS
+    try {
+      const customer = await stripe.customers.create({
+        email: sanitizedUser.email,
+        name: sanitizedUser.username,
+        address: {
+          line1: sanitizedUser.street,
+          city: sanitizedUser.city,
+          postal_code: sanitizedUser.zipcode,
+          state: sanitizedUser.state
+        },
+        phone: sanitizedUser.mobile_number,
+        metadata: {
+          user_id: sanitizedUser.id
+        }
+      });
 
-    ctx.send({
-      session,
-      customer,
-      sanitizedUser,
-      jwt
-    });
+      const session = await stripe.checkout.sessions.create({
+        mode: 'subscription',
+        customer: customer.id,
+        line_items: [{ price: process.env.STRIPE_TEST_MEMBERSHIP_PLAN_PRICE_ID, quantity: 1 }],
+        payment_method_types: ['paypal', 'card'],
+        payment_method_collection: 'always',
+        discounts: [{ coupon: process.env.STRIPE_TEST_MEMBERSHIP_PLAN_DISCOUNT_ID }],
+        currency: 'USD',
+        success_url: `${
+          process.env.SERVER_BASE_URL || 'http://localhost:1337'
+        }/api/auth/membership/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: 'http://localhost:1337',
+        metadata: {
+          user_id: sanitizedUser.id
+        }
+      });
+
+      await users.update({
+        where: {
+          email: sanitizedUser.email
+        },
+        data: {
+          stripe_customer_id: customer.id
+        }
+      });
+
+      const response = {
+        message: 'Your account has been successfully created!',
+        session_url: session.url,
+        session_id: session.id,
+        jwt,
+        user: sanitizedUser
+      };
+
+      ctx.send(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        strapi.log.error(error.message);
+        return ctx.badRequest(error.message, { error });
+      }
+    }
 
     services.createCartRelations(sanitizedUser);
-
-    users.update({
-      where: {
-        email: sanitizedUser.email
-      },
-      data: {
-        stripe_customer_id: customer.id
-      }
-    });
   },
 
   async onMembershipCheckoutSuccess(ctx: API.Context<null, API.Auth.MembershipCheckoutSuccessQuery>) {
